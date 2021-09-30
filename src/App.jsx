d561234c157b046228906710d169b6a167b05ed0
ism90 <ianmason5@gmail.com>
2021-09-30 16:23:27 +0100

    Imports

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+import Card from "./components/Card/Card";
+import CardList from "./components/CardList/CardList";
+import FilterItem from "./components/FilterItem/FilterItem";
+import FiltersList from "./components/FiltersList/FiltersList";
+import Main from "./components/Main/Main";
+import Navbar from "./components/Navbar/Navbar";
+import SearchBox from "./components/SearchBox/SearchBox";
+
+const App = () => {};
+
+export default App;
