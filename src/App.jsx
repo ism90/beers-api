0841c6865de02eb11c5bae71d43e1610bf9dd9b0
ism90 <ianmason5@gmail.com>
2021-10-06 12:48:24 +0100

    Api fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,44 @@
 import "./App.scss";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import FilterItem from "./components/FilterItem/FilterItem";
 import FilterList from "./components/FilterList/FilterList";
 import Main from "./components/Main/Main";
 import Navbar from "./components/Navbar/Navbar";
 import SearchBox from "./components/SearchBox/SearchBox";
-import beers from "./data/beers";
-
-
+// import beers from "./data/beers";
 
 const App = () => {
+  // State for Beers
+  const [beers, setBeers] = useState(0);
+
+  // State for Search
+
+  console.log("App refresh");
+  // Fetch for all Punk Beers
+  useEffect(() => {
+    console.log("useEffect run");
+    fetch("https://api.punkapi.com/v2/beers?page=2&per_page=80")
+      .then((res) => {
+        return res.json();
+      })
+      .then((beersArray) => {
+        console.log(beersArray);
+        return setBeers(beers);
+        // const mealsArray = mealsObj.meals;
+        // // to deal with error message from search
+        // if(mealsArray) {
+        //   setMeals(mealsArray);
+        // } else {setMeals([]);
+        // }
+      });
+  }, [beers]);
+
   return (
     <div className="app">
-      <Navbar />
+      {/* <Navbar />
 
-      <Main data={beers} />
+      <Main /> */}
     </div>
   );
 };
