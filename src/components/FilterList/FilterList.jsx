ffb6d002a149b886d2aad46996c2f698c2bf07b2
ism90 <ianmason5@gmail.com>
2021-10-07 17:44:19 +0100

    folder structure

--- src/components/FilterList/FilterList.jsx
+++ src/components/FilterList/FilterList.jsx
@@ -1,6 +1,9 @@
-import React from "react";
-import "./FilterList.scss";
-import FilterItem from "../FilterItem/FilterItem";
+import React from 'react'
+import "./FilterList.scss"
+import HighABV from './HighABV/HighABV'
+import ClassicRange from './ClassicRange/ClassicRange'
+import Acidic from './Acidic/Acidic'
+
 
 const FilterList = (props) => {
 
